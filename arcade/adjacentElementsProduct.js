286a8981971a234399a617eba37700e58b19dec6
Sacha Sedriks <sachased@gmail.com>
2020-12-06 14:04:54 -0800

    solutions from elsewhere

--- arcade/adjacentElementsProduct.js
+++ arcade/adjacentElementsProduct.js
@@ -9,7 +9,8 @@
 
 */
 
-function adjacentElementsProduct(inputArray) {
+// v1: my solution
+function adjacentElementsProduct1(inputArray) {
 
     let maxPairSum = 0;
     let maxPair = [];
@@ -38,6 +39,76 @@ function adjacentElementsProduct(inputArray) {
 }
 
 
+
+// SOLUTIONS ELSEWHERE ///////////////////////////
+
+// v2 Holy Shit!!!
+function adjacentElementsProduct2(inputArray) { 
+    return Math.max(...inputArray.slice(1).map((x,i)=>[x*inputArray[i]]));
+}
+
+// v2 DECONSTRUCTED
+// with .splice(n) of a .map(currentValue, index)
+// it shifts the starting index of the .map currentValue to `n` elements in the array
+// while maintainng the start of the original array index at 0.
+// Which allows the same array to be parsed at two different starting points!!!
+// Beyond me...
+// Math.max is pretty cool too.
+//
+// function adjacentElementsProduct(inputArray) { 
+//     // console.log(...inputArray.slice(1).map((x,i)=>[x*inputArray[i]]));
+//     console.log(inputArray);
+//     console.log(
+//         inputArray.slice(1).map((x,i) => {
+//             console.log('x value: ', x, 'index: ', i+1);
+//             console.log('i value: ', inputArray[i], 'index: ', i);
+//             console.log('inputArray[i]: ', );
+//             return x*inputArray[i];
+//         })
+//     );
+// }
+
+// v3
+// without the .slice(1) of v2, this makes more sense to me in how one would shift the index to get a different value in the array compared with the value of the head
+function adjacentElementsProduct3(inputArray) { 
+    // init
+    let largestPairSoFar = inputArray[0] * inputArray[1];
+    // transverse through array comparing
+    inputArray.map((value, index) => {
+        if (inputArray[index+1]) {
+            if (value * inputArray[index+1] > largestPairSoFar) {
+                largestPairSoFar = value * inputArray[index+1];
+            }
+        }
+    });
+    return largestPairSoFar;
+}
+
+
+// template
+// function adjacentElementsProductX(inputArray) { }
+
+
 // TEST DATA
-console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3])); // expected: 21
-console.log(adjacentElementsProduct([-23, 4, -3, 8, -12])); // expected: -12
+// console.log(adjacentElementsProduct([1, 2, 3, 4, 5])); // expected: 20
+// console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3])); // expected: 21
+// console.log(adjacentElementsProduct([-23, 4, -3, 8, -12])); // expected: -12
+
+// setting up a Handler, 
+// so can semi-dynamically invoke multiple functions
+const Handler = {
+    adjacentElementsProduct1,
+    adjacentElementsProduct2,
+    adjacentElementsProduct3,
+};
+function testSeveralVersions(num) {
+    for (let i = 1; i <= num; i++) {
+        console.log(`\n/////////////// adjacentElementsProduct${i}`)
+        console.log(Handler[`adjacentElementsProduct${i}`]([1, 2, 3, 4, 5]));   // expected: 20
+        console.log(Handler[`adjacentElementsProduct${i}`]([3, 6, -2, -5, 7, 3]));  // expected: 21
+        console.log(Handler[`adjacentElementsProduct${i}`]([-23, 4, -3, 8, -12])); // expected: -12
+    }
+}
+testSeveralVersions(3);
+
+
